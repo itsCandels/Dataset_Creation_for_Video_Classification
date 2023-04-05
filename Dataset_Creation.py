4c790d7ebf693ef9fe61cf501b6f67fd7859c597
Federico Candela <108625635+itsCandels@users.noreply.github.com>
2023-04-05 16:00:15 +0200

    Add files via upload

---
+++ Dataset_Creation.py
@@ -0,0 +1,260 @@
+
+@AUTHOR #ITS_CANDELS
+#05/04/2023
+#import the necessary packages
+
+
+import imutils
+import time
+import cv2
+from datetime import datetime as dt
+# import pandas as pd
+from moviepy.editor import VideoFileClip
+from pathlib import Path
+import os 
+start=time.time()
+
+
+#INITIALIZE BACKGROUND SUBTRACTOR
+
+fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
+
+
+#INIZIALIZE TIME
+
+start_time = time.time()    
+now=dt.now()
+now_time= now.strftime("%m_%d_%Y_%H:%M:%S")
+print("DATA E ORA:",now_time)
+elapsed=time.time()-start
+
+
+
+
+#PATH
+db= ("INPUT_VIDEO")
+
+
+#LIST
+lista_array=[]
+h=[]
+m=[]
+s=[]
+ms=[]
+lista_csv=[]
+
+
+
+#COUNTER
+count = 0
+cc=0
+frames=0
+
+
+lista_array.insert(0,0)
+
+
+captured = False
+
+#RENAME_ALL_FILES
+files = os.listdir(db)
+
+counter2=0
+for index, file in enumerate(files):
+    counter2+=1
+    dir=os.rename(os.path.join(db, file), os.path.join(db, 'video' .join([str(counter2), '.mp4'])))
+
+
+pathvideo= Path(db).glob('**/*.mp4')
+data_dir_list = list(pathvideo)
+
+
+for i in data_dir_list:
+    cc+=1
+    change_frame = 0
+    candels=0
+    frames=0
+
+
+    
+
+    d=str(i)
+    
+
+    path=os.path.dirname(d)
+    
+
+    print('NEW___VIDEO___NAME___ = '+str(path)+'/'+'video '+'('+str(cc)+')'+'.mp4')
+
+    
+    
+    
+    
+
+    cap = cv2.VideoCapture(d)
+    width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print('FPS NUMBER: '+str(fps))
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print('FRAMES NUMBER: '+str(frame_count))
+    (W, H) = (None, None)
+    
+    
+        
+
+    while True:
+        # grab a frame from the video
+
+        (grabbed, frame) = cap.read()
+    
+        # if the frame is None, then we have reached the end of the
+        # video file
+        change_frame +=1
+        
+        if frame is None:
+            break
+    	
+        #else: 
+        # clone the original frame (so we can save it later), resize the
+    	# frame, and then apply the background subtractor
+        orig = frame.copy()
+        frame = imutils.resize(frame, width=600)
+        mask = fgbg.apply(frame)
+        
+        # apply a series of erosions and dilations to eliminate noise
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+        
+        # if the width and height are empty, grab the spatial dimensions
+        if W is None or H is None:
+            (H, W) = mask.shape[:2]
+            
+            
+            
+    	
+        #TIMESTAMP
+        milliseconds = cap.get(cv2.CAP_PROP_POS_MSEC)
+    
+        seconds = int(milliseconds//1000)
+        milliseconds =milliseconds%1000
+        minutes = 0
+        hours = 0
+        
+        if hours >= 60:
+            minutes = int(hours//60)
+            hours = int(hours % 60)
+    
+        if seconds >= 60:
+    
+            minutes =int(seconds//60)
+            seconds = int(seconds % 60)
+    
+        if minutes >= 60:
+            hours = int(minutes//60)
+            minutes = int(minutes % 60)
+            
+        ore=(f'{hours:02}')
+        minuti=(f'{minutes:02}')
+        secondi=(f'{seconds:02}')
+            
+        timestamp=(f'{hours:02}'+':'+f'{minutes:02}'+':'+f'{seconds:02}')
+        
+        
+        
+        
+        
+        # compute the percentage of the mask that is "foreground"
+        p = (cv2.countNonZero(mask) / float(W * H)) * 100
+    
+
+    
+        if p < 0.50 and not captured and frames > int(fps*2):
+            lista_array.append(change_frame)
+            u=[x - abs(lista_array[i-1]) if i else None for i, x in enumerate(lista_array)][1:]           
+            diff=u[candels]
+
+            
+            durata=abs(diff/fps)
+            
+            start=change_frame/fps
+            endtime=start+durata
+
+            count+=1
+                        #INSERT_OUTPUT_PATH_HERE
+            outputfile='OUTPUT_FILE_MINIVIDEO/VIDEO_N_'+str(cc)+'_SCENA_'+str(count)+'.mp4'                        
+            required_video_file=VideoFileClip('INPUT_VIDEO/'+str(cc)+'video.'+'mp4')            
+           
+            full_duration=required_video_file.duration
+            
+            #ZERO_SHOT_LEARNING
+            
+            
+            
+            if start > full_duration:
+                print(f"video too short to get {start}-{endtime} (full duration: {full_duration})")
+                break  
+            
+            if endtime > full_duration:
+                print(f"crop endtime {endtime} to {full_duration}")
+                endtime = full_duration
+            
+            
+            
+            clip =  required_video_file.subclip (start, endtime)
+            clip.to_videofile (outputfile, codec="libx264", temp_audiofile='temp-audio'+str(cc)+'.m4a', remove_temp=True, audio_codec='aac')
+
+            candels+=1
+            
+
+            print('')
+            print('TOTAL_VIDEO_DURATION '+str(full_duration))
+            print('')
+            print('VIDEO_NAME '+('VIDEO_ELABORATION/'+'video '+'('+str(cc)+')'+'.mp4'))
+            print('')
+            print('START: ' +str(start))
+            print('SCENA N°'+ str(count))
+            print('H:M:S '+timestamp) 
+            
+            print('END: ' +str(endtime))
+            print('')
+            print('DURATION_MINIVIDEO: '+str(durata))
+            print('')
+    
+            captured = True       
+
+    	
+            lista_csv.append(timestamp)
+        # otherwise, either the scene is changing or we're still in warmup
+    	# mode so let's wait until the scene has settled or we're finished
+    	# building the background model
+        elif captured and p >=15:
+              
+            captured = False
+            
+
+        key = cv2.waitKey(1) & 0xFF
+    	
+        # if the `x` key was pressed, break from the loop
+        if key == ord("x"):
+            break
+    	
+        # increment the frames counter
+        frames += 1
+        # do a bit of cleanup
+    
+
+    
+    
+    
+    cap.release()
+   
+    # lista = pd.DataFrame(lista_csv)
+    # lista.to_csv(r'OUTPUT_PAZIENTI_NORMALI/Totalvideo.csv', header=False, index=False)    
+    
+    
+    #FINE DEI TEMPI DI ELABORAZIONE
+    elapsed=time.time()-start
+    output=dt.strftime(dt.utcfromtimestamp(elapsed), '%H:%M:%S')
+    print("TEMPOELABORAZIONE:",output)
